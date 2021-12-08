7da7eaa605ce73c3accc176e2281f8e691383777
Luis Eduardo Ribeiro <luiseduardo.sribeiro@gmail.com>
2021-12-08 00:05:47 -0300

    Config database heroku

--- src/database/config/sequelizeConnection.ts
+++ src/database/config/sequelizeConnection.ts
@@ -5,4 +5,19 @@ const username = config.username as string
 const password = config.password as string
 const database = config.database as string
 
-export default new Sequelize(database, username, password, config as Options)
+let sequelize = null
+
+if (process.env.DATABASE_URL) {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        dialectOptions: {
+            ssl: true
+        }
+    })
+}
+else {
+    sequelize = new Sequelize(database, username, password, config as Options)
+}
+
+export default sequelize
