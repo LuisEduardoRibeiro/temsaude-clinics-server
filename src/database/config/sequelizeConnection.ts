ca282d6a0b659579dea9823113c992553168809c
Luis Eduardo Ribeiro <luiseduardo.sribeiro@gmail.com>
2021-12-09 08:46:48 -0300

    Ajuste db connection

--- src/database/config/sequelizeConnection.ts
+++ src/database/config/sequelizeConnection.ts
@@ -5,12 +5,4 @@ const username = config.username as string
 const password = config.password as string
 const database = config.database as string
 
-const sequelizeConnection = new Sequelize(process.env.DATABASE_URL as string, {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-        ssl: true
-    }
-})
-
-export default sequelizeConnection
+export default new Sequelize(database, username, password, config as Options)
